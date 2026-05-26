1ad3d12e11a1fc7ca104b989e488b636f4d44f1c
Jeny Sherlyn Raj <jenysherlynraj2005@gmail.com>
2026-05-26 10:23:07 +0530

    Initialize ASP.NET Core application with CORS support

---
+++ Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazor",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowBlazor");
+
+app.MapControllers();
+
+app.Run();
